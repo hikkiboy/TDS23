a1f336ef4ffdb577b97067e9546df434c3a6e2aa
GUSTAVO HENRIQUE TORRES E SILVA <3266.343@edu.fb.org.br>
2023-05-22 21:40:01 -0300

    Adicionado Script Pubs

--- SQL/aula2.sql
+++ SQL/aula2.sql
@@ -64,6 +64,40 @@ DELETE FROM authors WHERE au_id = '172-32-1177'
 -- seleciona
 Select * from authors where au_id = '172-32-1177'
 
+--Left join
+
+ SELECT a.au_id as "Id do Autor",a.au_lname as "Nome do Autor",
+a.au_fname as "Sobrenome do autor",t.title_id as "Id do Titulo",
+t.au_ord as Ordem,t.royaltyper as "Tipo de Royalt"
+FROM authors a LEFT JOIN titleauthor t ON a.au_id = t.au_id
 
 
+--Right join
+SELECT a.au_id as "Id do Autor",a.au_lname as "Nome do Autor",
+a.au_fname as "Sobrenome do autor", t.title_id as "Id do Titulo",
+t.au_ord as Ordem,t.royaltyper as "Tipo de Royalt"
+FROM authors a RIGHT JOIN titleauthor t ON a.au_id = t.au_id
+
+-- fluxo join
+SELECT a.au_id as "Id do Autor",a.au_lname as "Nome do Autor",
+a.au_fname as "Sobrenome do autor",t.title_id as "Id do Titulo",
+t.au_ord as Ordem,t.royaltyper as "Tipo de Royalt"
+FROM authors a FULL OUTER JOIN titleauthor t ON a.au_id = t.au_id
+
+--cro joi
+
+SELECT a.au_id as "Id do Autor",a.au_lname as "Nome do Autor",
+a.au_fname as "Sobrenome do autor",t.title_id as "Id do Titulo",
+t.au_ord as Ordem,t.royaltyper as "Tipo de Royalt"
+FROM authors a CROSS JOIN titleauthor t
+
+--Subi queria
+
+SELECT * FROM titles WHERE pub_id IN (SELECT TOP(2) pub_id FROM
+publishers)
+
+--subi queria 2
+
+SELECT sum(price) as "Total vendido por este autor" FROM titles
+WHERE pub_id IN (SELECT TOP(1) pub_id FROM publishers)
 

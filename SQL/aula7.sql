20592fde2fb850f432607b5c8de2de2269061580
hikki <3266.343@edu.fb.org.br>
2023-05-30 21:42:48 -0300

    aula 7

---
+++ SQL/aula7.sql
@@ -0,0 +1,27 @@
+ALTER VIEW nOMES as 
+SELECT FIRSTNAME nome, Middlename nomeDoMeio, LastName ultimoNome from person.Person 
+
+select * into Nome from nOMES
+
+SELECT * from Nome
+
+
+
+alter View Nome_e_email as 
+
+
+SELECT FirstName, EmailAddress into tblTESTE from nome_e_email
+
+ALTER TABLE tblTESTE 
+ADD idDeTeste int PRIMARY KEY identity
+
+select * from tblteste
+
+UPDATE TBLteste
+SET FirstName = 'MARIA'
+
+DELETE FROM tblteste
+WHERE Iddeteste = 1
+
+DROP TABLE TblTeste
+

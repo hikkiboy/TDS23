14140850c875eb52fbd9ca67cc5f8980e983a349
hikki <3266.343@edu.fb.org.br>
2023-07-20 21:42:40 -0300

    Spring dnv pog

--- JAVASTS4/blog/src/main/java/com/fundacaobradesco/blog/controller/PostagemController.java
+++ JAVASTS4/blog/src/main/java/com/fundacaobradesco/blog/controller/PostagemController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,12 @@ public class PostagemController {
 	@GetMapping
 	public ResponseEntity<List<Postagem>> GetAll(){
 		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> GetById(@PathVariable Long id){
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+		
 	}
 
 }

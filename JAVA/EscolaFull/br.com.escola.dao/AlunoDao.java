fd2a4fa4c4e0c338f65bafc01a99eba4d060042f
hikki <3266.343@edu.fb.org.br>
2023-08-02 21:43:08 -0300

    ultimo eu acho na real vai ter mais um

--- JAVA/EscolaFull/br.com.escola.dao/AlunoDao.java
+++ JAVA/EscolaFull/br.com.escola.dao/AlunoDao.java
@@ -1,10 +1,24 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class AlunoDao extends ConDao {
 	public void alterar(Aluno aluno){
-		Connection conexao = getConnection();
-		PreparedStatement pstmt = conexao.prepareStatement("UPDATE tbAluno SET nome=?, telefone = ?, email = ?,datacadastro = ?" + "WHERE matricula=?" );
+		try {
+			Connection conexao = getConnection();
+			PreparedStatement pstmt = conexao.prepareStatement("UPDATE tbAluno SET nome=?, telefone = ?, email = ?,datacadastro = ?" + "WHERE matricula=?" );
+			pstmt.setString(1, aluno.getNome());
+			pstmt.setString(2, aluno.getTelefone());
+			pstmt.setString(3, aluno.getEmail());
+			pstmt.setDate(4, new java.sql.Date(aluno.getDataCadastro().getTime()));
+			pstmt.setLong(5, aluno.getMatricula());
+			pstmt.execute();
+			pstmt.close();
+			conexao.close();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
 		
 	}
 }
